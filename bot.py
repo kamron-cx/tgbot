e8c242e8a50e82a500d2e2c21ea7aaa1f426622c
kamron_cx <kamroncx@gmail.com>
2026-03-15 01:43:33 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telebot import types
 import json
 import os
 
-TOKEN = "7961600141:AAHkKdsq1bblalGv1BruRzPaBFkqb8oGnxs"
+TOKEN = "8633260476:AAFlCH9VjAX5ftfd4emmxR89as661R_EccE"
 bot = telebot.TeleBot(TOKEN)
 
 DB_FILE = "database.json"
